a801389a8873215c433afa78441f27ee2e5be03e
derekrsargent <derek.r.sargent@gmail.com>
2022-01-03 19:57:11 -0500

    Init commit

---
+++ routes/api.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+router.get('/api/ping', (req, res, next) => {
+    res.status(200).json({ "success": "true" });
+});
+
+router.get('/api/posts', async (req, res, next) => {
+    const queryObject = req.query; 
+    console.log(queryObject);
+    let tags = [];
+    let posts = [];
+    let sortBy = 'id',
+        direction = 'asc';
+
+    if(!queryObject.tag) {
+        return res.status(400).json({ "error": "Tags parameter is required" });
+    };
+
+    if(queryObject.sortBy && 
+        queryObject.sortBy !== 'id' &&
+        queryObject.sortBy !== 'reads' && 
+        queryObject.sortBy !== 'likes' && 
+        queryObject.sortBy !== 'popularity'
+    ) {
+        return res.status(400).json({ "error": "sortBy parameter is invalid" });
+    } else {
+        sortBy = queryObject.sortBy; 
+    };
+
+    if(queryObject.direction &&
+        queryObject.direction !== 'asc' && 
+        queryObject.direction !== 'desc'
+    ) {
+        return res.status(400).json({ "error": "direction parameter is invalid" });
+    } else {
+        direction = queryObject.direction;
+    };
+
+    tags = queryObject.tag.split(',');
+    let responses = tags.map(tag => axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}`));
+
+    const checkIfExists = (postId, arr) => {
+        return arr.some(({id}) => id === postId);
+    };
+
+    try {
+        const response = await Promise.all(responses);
+
+        response
+            .map(postsByTag => postsByTag.data.posts
+                .map(el => !checkIfExists(el.id, posts) && posts.push(el)));
+   
+    } catch (err) {
+        return res.status(500).json({ "error": err.message });
+    };
+
+    const customSort= (property) => {
+        const sortDirection = direction === 'asc' ? 1 : -1;
+        
+        return function (a,b) {
+            const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortDirection;
+        }
+    }
+
+    posts.sort(customSort(sortBy));
+
+    res.status(200).json({ "posts": posts });
+});
+
+module.exports = router;
